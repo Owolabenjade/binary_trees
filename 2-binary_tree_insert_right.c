60f40ab1d6c7a37f07fbb592581cb4c2f9899b53
Benjamin <owolabenjade@gmail.com>
2024-04-06 15:48:24 -0700

    matter

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,25 +7,25 @@
  * @value: Value to store in the new node
  *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
- */
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        new_node->right = parent->right; /* Existing right-child becomes right-child of new node */
-        parent->right->parent = new_node; /* Update parent of existing right-child */
-    }
-    parent->right = new_node; /* Set new_node as parent's right-child */
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right; /* Becomes right-child of new node */
+		parent->right->parent = new_node; /* Update parent of existing right-child */
+	}
+	parent->right = new_node; /* Set new_node as parent's right-child */
 
-    return (new_node);
+	return (new_node);
 }
 
