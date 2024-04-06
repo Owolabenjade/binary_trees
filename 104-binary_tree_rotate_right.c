ed8fa262eefc2b9a41163f9a8b519c3936bde49f
Benjamin <owolabenjade@gmail.com>
2024-04-06 14:21:16 -0700

    eyah

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -6,23 +6,23 @@
  * @tree: Pointer to the root node of the tree to rotate.
  *
  * Return: Pointer to the new root node after rotation.
- */
+*/
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *new_root, *temp;
+	binary_tree_t *new_root, *temp;
 
-    if (tree == NULL || tree->left == NULL)
-        return (tree); /* Cannot rotate */
+	if (tree == NULL || tree->left == NULL)
+		return (tree); /* Cannot rotate */
 
-    new_root = tree->left;
-    temp = new_root->right;
-    new_root->right = tree; 
-    tree->left = temp;
+	new_root = tree->left;
+	temp = new_root->right;
+	new_root->right = tree;
+	tree->left = temp;
 
-    /* Update parents if necessary */
-    if (temp)
-        temp->parent = tree;
+	/* Update parents if necessary */
+	if (temp)
+		temp->parent = tree;
 
-    return (new_root); 
+	return (new_root);
 }
 
